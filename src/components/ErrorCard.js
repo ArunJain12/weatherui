68aaf827c93eb23ad5bbbc246e267883e7b82a3d
ArunJain12 <39081167+ArunJain12@users.noreply.github.com>
2019-12-19 01:10:12 +0530

    Update ErrorCard.js

--- src/components/ErrorCard.js
+++ src/components/ErrorCard.js
@@ -7,8 +7,8 @@ function ErrorCard() {
             <div className="card text-white bg-danger mb-3" style={{ maxWidth: '48rem' }}>
                 <div className="card-header">Error from Yahoo API</div>
                 <div className="card-body">
-                    <h5 className="card-title">Danger card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 className="card-title">Yahoo API Error</h5>
+                    <p className="card-text">Error while calling Yahoo data api.</p>
                 </div>
             </div>
             <Link to="/" className="btn btn-primary">Go back to Weather Search </Link>
